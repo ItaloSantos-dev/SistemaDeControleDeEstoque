b03bfb1e732ab41949207eb240565bbff2f7f23f
Italo Santos <italosantoscont@gmail.com>
2025-10-03 13:22:27 -0300

    Criação das migrations

---
+++ database/migrations/2025_10_03_155916_create_variacoes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('variacoes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('produto_id');
+            $table->decimal('preco', 6,2);
+            $table->decimal('quantidade', 6,2);
+            $table->string('unidade');
+            $table->integer('estoque');
+            $table->timestamps();
+
+            $table->foreign('produto_id')->references('id')->on('produtos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('variacoes');
+    }
+};
